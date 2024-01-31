8625d6a81cccb35cafc0b4ea3d38d5aa3f116efd
carolangel19 <102700849+CarolAngel19@users.noreply.github.com>
2024-01-31 15:44:48 -0500

    Laboratorio Directivas Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  
+  public muestras = [
+    'Muestra 1',
+    'Muestra 2',
+    'Muestra 3',
+    'Muestra 4',
+    'Muestra 5',
+  ]
+  
+}
